50969e3d25bf83b787dea488465a86179c419d49
MayurB1007 <mayurbhabad@gmail.com>
2024-10-31 02:14:16 -0400

    CLEANUP FILE
    Cleanup file script for all Tables

--- cleanup.sql
+++ cleanup.sql
@@ -1,114 +1,175 @@
--- Clean up script file
+-- CLEAN UP SCRIPT FILE
+
 -- "USERS" TABLE
 SET SERVEROUTPUT ON;
-DECLARE TABLE_EXISTS NUMBER;
 BEGIN
-     SELECT COUNT(*)
-     INTO TABLE_EXISTS
-     FROM USER_TABLES
-  WHERE TABLE_NAME = 'USERS';
- IF 
-    TABLE_EXISTS > 0
-    THEN 
-     dbms_output.put_line ('USERS TABLE WILL BE DROPPED');
     EXECUTE IMMEDIATE 'DROP TABLE USERS';
-  ELSE
-     dbms_output.put_line ('GOOD TO GO TO NEXT STEP');
-END IF;
+    dbms_output.put_line(' TABLE USERS DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
 END;
 /
 
 -- "CANDIDATES" TABLE
 SET SERVEROUTPUT ON;
-DECLARE TABLE_EXISTS NUMBER;
 BEGIN
-     SELECT COUNT(*)
-     INTO TABLE_EXISTS
-     FROM USER_TABLES
-  WHERE TABLE_NAME = 'CANDIDATES';
- IF 
-    TABLE_EXISTS > 0
-    THEN 
-     dbms_output.put_line ('CANDIDATES TABLE WILL BE DROPPED');
     EXECUTE IMMEDIATE 'DROP TABLE CANDIDATES';
-  ELSE
-     dbms_output.put_line ('GOOD TO GO TO NEXT STEP');
-END IF;
+    dbms_output.put_line(' TABLE CANDIDATES DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
 END;
 /
 
 -- "WORK_EXP" TABLE
 SET SERVEROUTPUT ON;
-DECLARE TABLE_EXISTS NUMBER;
 BEGIN
-     SELECT COUNT(*)
-     INTO TABLE_EXISTS
-     FROM USER_TABLES
-  WHERE TABLE_NAME = 'WORK_EXP';
- IF 
-    TABLE_EXISTS > 0
-    THEN 
-     dbms_output.put_line ('WORK_EXP TABLE WILL BE DROPPED');
     EXECUTE IMMEDIATE 'DROP TABLE WORK_EXP';
-  ELSE
-     dbms_output.put_line ('GOOD TO GO TO NEXT STEP');
-END IF;
+    dbms_output.put_line(' TABLE WORK_EXP DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
 END;
 /
 
 -- "COMPANIES" TABLE
 SET SERVEROUTPUT ON;
-DECLARE TABLE_EXISTS NUMBER;
 BEGIN
-     SELECT COUNT(*)
-     INTO TABLE_EXISTS
-     FROM USER_TABLES
-  WHERE TABLE_NAME = 'COMPANIES';
- IF 
-    TABLE_EXISTS > 0
-    THEN 
-     dbms_output.put_line ('COMPANIES TABLE WILL BE DROPPED');
     EXECUTE IMMEDIATE 'DROP TABLE COMPANIES';
-  ELSE
-     dbms_output.put_line ('GOOD TO GO TO NEXT STEP');
-END IF;
+    dbms_output.put_line(' TABLE COMPANIES DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
 END;
 /
 
 -- "RECRUITERS" TABLE
 SET SERVEROUTPUT ON;
-DECLARE TABLE_EXISTS NUMBER;
 BEGIN
-     SELECT COUNT(*)
-     INTO TABLE_EXISTS
-     FROM USER_TABLES
-  WHERE TABLE_NAME = 'RECRUITERS';
- IF 
-    TABLE_EXISTS > 0
-    THEN 
-     dbms_output.put_line ('RECRUITERS TABLE WILL BE DROPPED');
     EXECUTE IMMEDIATE 'DROP TABLE RECRUITERS';
-  ELSE
-     dbms_output.put_line ('GOOD TO GO TO NEXT STEP');
-END IF;
+    dbms_output.put_line(' TABLE RECRUITERS DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
 END;
 /
 
 -- "SKILLS" TABLE
 SET SERVEROUTPUT ON;
-DECLARE TABLE_EXISTS NUMBER;
 BEGIN
-     SELECT COUNT(*)
-     INTO TABLE_EXISTS
-     FROM USER_TABLES
-  WHERE TABLE_NAME = 'SKILLS';
- IF 
-    TABLE_EXISTS > 0
-    THEN 
-     dbms_output.put_line ('SKILLS TABLE WILL BE DROPPED');
     EXECUTE IMMEDIATE 'DROP TABLE SKILLS';
-  ELSE
-     dbms_output.put_line ('GOOD TO GO TO NEXT STEP');
-END IF;
+    dbms_output.put_line(' TABLE SKILLS DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
 END;
 /
+
+-- "EDUCATION" TABLE
+SET SERVEROUTPUT ON;
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE EDUCATION';
+    dbms_output.put_line(' TABLE EDUCATION DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
+END;
+/
+
+-- "JOB_REQUISITION" TABLE
+SET SERVEROUTPUT ON;
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE JOB_REQUISITION';
+    dbms_output.put_line(' TABLE JOB_REQUISITION DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
+END;
+/
+
+-- "ADDRESS" TABLE
+SET SERVEROUTPUT ON;
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE ADDRESS';
+    dbms_output.put_line(' TABLE ADDRESS DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
+END;
+/
+
+-- "RESUME" TABLE
+SET SERVEROUTPUT ON;
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE RESUME';
+    dbms_output.put_line(' TABLE RESUME DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
+END;
+/
+
+-- "JOB_REQUISITION" TABLE
+SET SERVEROUTPUT ON;
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE JOB_REQUISITION';
+    dbms_output.put_line(' TABLE JOB_REQUISITION DROPPED SUCCESSFULLY ');
+EXCEPTION
+    WHEN OTHERS THEN
+        IF sqlcode = -942 THEN
+        dbms_output.put_line(' GOOD TO GO TO NEXT SCRIPT ');
+        ELSE
+        RAISE;
+        END IF;
+END;
+/
+
+
+
+
+
+
+
+
+
