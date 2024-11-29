13224c41e86eb2fdc701d7d64964a8141c5d7f28
MayurB1007 <mayurbhabad@gmail.com>
2024-11-28 20:22:52 -0500

    Updates with all testing scripts

--- packages/pkg_can_user_mgmt.sql
+++ packages/pkg_can_user_mgmt.sql
@@ -64,6 +64,9 @@ CREATE OR REPLACE PACKAGE BODY PKG_CANDIDATE_USER_MANAGEMENT AS
         INVALID_AGE EXCEPTION;
         INVALID_VETERAN EXCEPTION;
         INVALID_DISABILITY EXCEPTION;
+        DUPLICATE_EMAIL EXCEPTION;
+        DUPLICATE_USERNAME EXCEPTION;
+        V_COUNT NUMBER;
         
     BEGIN
         IF PI_FNAME IS NULL THEN
@@ -120,6 +123,26 @@ CREATE OR REPLACE PACKAGE BODY PKG_CANDIDATE_USER_MANAGEMENT AS
             THEN RAISE INVALID_DISABILITY;
         END IF;
         
+        --For Uniqueness check of email 
+        SELECT COUNT(*)
+        INTO V_COUNT
+        FROM USERS
+        WHERE EMAIL = PI_EMAIL;
+
+        IF V_COUNT > 0 THEN
+            RAISE DUPLICATE_EMAIL;
+        END IF;
+        
+        --For Uniqueness check of username
+        SELECT COUNT(*)
+        INTO V_COUNT
+        FROM USERS
+        WHERE USERNAME = PI_UNAME;
+
+        IF V_COUNT > 0 THEN
+            RAISE DUPLICATE_USERNAME;
+        END IF;
+        
         -- Insert into USERS table
         INSERT INTO USERS (FIRSTNAME, LASTNAME, EMAIL, USERNAME, PASSWORD)
         VALUES (PI_FNAME, PI_LNAME, PI_EMAIL, PI_UNAME, PI_PW)
@@ -134,38 +157,54 @@ CREATE OR REPLACE PACKAGE BODY PKG_CANDIDATE_USER_MANAGEMENT AS
         INSERT INTO CANDIDATES (USER_ID, ADD_ID, PHONE, AGE, GENDER)
         VALUES (V_USER_ID, V_ADDRESS_ID, PI_PHONE, PI_AGE, PI_GENDER);
     
-        DBMS_OUTPUT.PUT_LINE('Candidate profile created successfully. User ID: ' || V_USER_ID);
+        UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Candidate profile created successfully. User ID: ' || V_USER_ID));
     EXCEPTION
         WHEN NULL_FNAME THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: First name cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: First name cannot be empty.'));
         WHEN NULL_LNAME THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Last name cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Last name cannot be empty.'));
         WHEN NULL_EMAIL THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Email cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Email cannot be empty.'));
         WHEN NULL_USERNAME THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Username cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Username cannot be empty.'));
         WHEN NULL_PASSWORD THEN 
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Password cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Password cannot be empty.'));
         WHEN NULL_ST1 THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Street 1 cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Street 1 cannot be empty.'));
         WHEN NULL_CITY THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: City cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: City cannot be empty.'));
         WHEN NULL_STATE THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: State cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: State cannot be empty.'));
         WHEN NULL_COUNTRY THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Country cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Country cannot be empty.'));
         WHEN INVALID_GENDER THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Gender must be "male" or "female".');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Gender must be "male" or "female".'));
         WHEN INVALID_AGE THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: Age must be greater than 16.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Age must be greater than 16.'));
         WHEN INVALID_VETERAN THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: 
-            Veteran status must be in "do not wish to disclose" or "not a veteran" or "I am a protected veteran".');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: 
+            Veteran status must be in "do not wish to disclose" or "not a veteran" or "I am a protected veteran".'));
         WHEN INVALID_DISABILITY THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: 
-            Disability status must be in "do not wish to disclose" or "no, i do not have a disability" or "yes, I have a disability"');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: 
+            Disability status must be in "do not wish to disclose" or "no, i do not have a disability" or "yes, I have a disability"'));
+            
+        WHEN DUPLICATE_EMAIL THEN
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: This Email already exists. Please try a different email')); 
+        
+        WHEN DUPLICATE_USERNAME THEN
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: This username already exists. Please try a different username'));
+            
+      --For Uniqueness check on email and username 
+      /*  WHEN DUP_VAL_ON_INDEX THEN
+            IF SQLERRM LIKE '%USERS_EMAIL%' THEN
+                UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: This Email already exists. Please try a different Email.'));
+            ELSIF SQLERRM LIKE '%USERS_USERNAME%' THEN
+                UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: This Username already exists. Please try a different Username.'));
+            ELSE
+                UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: Duplicate value encountered. ' || SQLERRM));
+            END IF;    */
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('Error creating candidate user profile: ' || SQLERRM);
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error creating candidate user profile: ' || SQLERRM));
             ROLLBACK;
     END CREATE_CANDIDATE_PROFILE;
 
@@ -189,17 +228,17 @@ CREATE OR REPLACE PACKAGE BODY PKG_CANDIDATE_USER_MANAGEMENT AS
         );
         
         IF SQL%ROWCOUNT = 0 THEN
-            DBMS_OUTPUT.PUT_LINE('Candidate not found.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Candidate not found.'));
         ELSE
-            DBMS_OUTPUT.PUT_LINE('Candidate account deactivated successfully.User ID: ' || V_CANDIDATE_ID);
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Candidate account deactivated successfully.User ID: ' || V_CANDIDATE_ID));
         END IF;
         
         COMMIT;
     EXCEPTION
         WHEN NULL_CANDIDATE_ID THEN
-            DBMS_OUTPUT.PUT_LINE('Error deactivating candidate account: Candidate ID cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error deactivating candidate account: Candidate ID cannot be empty.'));
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('Error deactivating candidate account: ' || SQLERRM);
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error deactivating candidate account: ' || SQLERRM));
             ROLLBACK;
     END DEACTIVATE_CANDIDATE;
 
@@ -225,15 +264,15 @@ CREATE OR REPLACE PACKAGE BODY PKG_CANDIDATE_USER_MANAGEMENT AS
         
     EXCEPTION
         WHEN NULL_CANDIDATE_ID THEN
-            DBMS_OUTPUT.PUT_LINE('Error in checking candidate account status: Candidate ID cannot be empty.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error in checking candidate account status: Candidate ID cannot be empty.'));
             RETURN 0;
             
         WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('Error in checking candidate account status: Candidate not found.');
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error in checking candidate account status: Candidate not found.'));
             RETURN 0;
             
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('Error in checking candidate account status:: ' || SQLERRM);
+            UTIL_PKG.ADD_NEW_LINE(UTIL_PKG.ADD_TAB('Error in checking candidate account status:: ' || SQLERRM));
             RETURN 0;
     END IS_CANDIDATE_ACTIVE;
 END PKG_CANDIDATE_USER_MANAGEMENT;
